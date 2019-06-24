d6d3a0b398969d381979588af48e41958b5b4b11
David H <davidhamberlin@gmail.com>
2019-06-24 14:37:42 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import WindowPortal from './WindowPortal';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    counter: 0,
+    showWindowPortal: false
+  }
+
+  toggleWindowPortal = () => {
+    this.setState(({ showWindowPortal }) => ({ showWindowPortal: !showWindowPortal }));
+  }
+  
+  increment = () => this.setState(({ counter }) => ({ counter: counter + 1 }))
+  decrement = () => this.setState(({ counter }) => ({ counter: counter - 1 }))
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Counter: {this.state.counter}</h1>
+          <div>
+            <button onClick={this.increment}>
+              Increment Counter
+            </button>
+            <button onClick={this.decrement}>
+              Decrement Counter
+            </button>
+          </div>
+          <button
+            onClick={this.toggleWindowPortal}
+          >
+            {this.state.showWindowPortal ? 'Close the' : 'Open a'} Portal
+          </button>
+          {this.state.showWindowPortal && (
+            <WindowPortal>
+              <h1>Counter in a portal: {this.state.counter}</h1>
+              <p>State is shared!</p>
+              <p>We can even increment/decrement from here.</p>
+              <button onClick={() => {
+                console.log('click')
+                this.increment()
+              }}>
+                Increment Counter
+              </button>
+              <button onClick={this.decrement}>
+                Decrement Counter
+              </button>
+            </WindowPortal>
+          )}
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
